bde54437105f4cd5d00d8016902a2dbc75e17595
John Scully <j0hns@users.noreply.github.com>
2015-02-26 21:25:14 +0000

    Rest of the projects to date

---
+++ Norris/src/main.c
@@ -0,0 +1,43 @@
+/*
+A simple test of serial-port functionality.
+Takes in a character at a time and sends it right back out,
+ displaying the ASCII value on the LEDs.
+*/
+
+
+# define F_CPU 1600000UL
+// ------- Preamble -------- //
+#include <avr/io.h>
+#include <util/delay.h>
+#include "pinDefines.h"
+#include "USART/USART.h"
+#include "minUnit/minunit.h"
+
+int run_norris();
+
+int main(void) {
+  //return run_tests();
+  return run_norris();
+}
+
+int run_norris()
+{
+	char serialCharacter;
+
+	// -------- Inits --------- //
+	LED_DDR = 0xff;                            /* set up LEDs for output */
+	initUSART();
+	printString("Hello World!\r\n");                          /* to test */
+
+	// ------ Event loop ------ //
+	while (1) {
+
+		serialCharacter = receiveByte();
+		transmitByte(serialCharacter);
+		LED_PORT = serialCharacter;
+		/* display ascii/numeric value of character */
+
+		}                                                  /* End event loop */
+		return (0);
+	
+}
