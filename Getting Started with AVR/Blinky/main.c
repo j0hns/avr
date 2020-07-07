b3a6f9927099f937f60112fa8aabed5fa165e2a3
John Scully <j0hns@users.noreply.github.com>
2020-07-07 21:59:20 +0100

    Now it blinks

--- Getting Started with AVR/Blinky/main.c
+++ Getting Started with AVR/Blinky/main.c
@@ -4,18 +4,24 @@
  * Created: 07/07/2020 21:24:13
  * Author : John
  */ 
+#define F_CPU 8000000UL //8Mhz assumes fuse CKDIV8 set low
 
 #include <avr/io.h>
+#include <util/delay.h> 
 
 //for use with OneDrive\Documents\Maker\AVR ISP Programmer\ZIF Programmer.fzz
+//Led on pin 14 = PB0
 
 int main(void)
 {
-    DDRB = (1<<DDB0);
+	
+    DDRB = (1<<DDB0); // Data direction register - Set PB0 high for output
+	PORTB |= (1<<PORTB0);//set pin 14 (PB0) high - switch on
+	
     while (1) 
     {
-		//set pin 14 (PB0) high
-		PORTB |= (1<<PORTB0);
+		PINB |= (1<<PINB0); //toggle PB0
+		_delay_ms(500);
     }
 }
 
