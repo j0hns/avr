a2c8f408f2b0c6a565080d9f934ce321d5f58012
John Scully <j0hns@users.noreply.github.com>
2020-07-27 22:25:00 +0100

    USART with interrupts

--- Getting Started with AVR/AVR_GPIO/main.c
+++ Getting Started with AVR/AVR_GPIO/main.c
@@ -5,12 +5,13 @@
 * Author : John
 */
 
+//#pragma GCC optimize ("O0") //Enable for simulator local variable debugging
+
 //Using timer interrupts
 
 #define F_CPU 8000000UL //8Mhz assumes fuse CKDIV8 set low
 
 #include <avr/io.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
 
 
@@ -25,15 +26,65 @@
 
 ISR(TIMER1_COMPA_vect)
 {
-	LED_PB0_TOGGLE;
+	LED_PB0_ON;
 }
 
-void Set_Timer1_Interrupt_Frequency(uint8_t freqHz)
+ISR(TIMER1_COMPB_vect)
 {
-	TCCR1B |= (1<<CS12) | (1<<WGM12); // Timer/Counter Control Register B = clkI/O/256 (From prescaler) |  Clear Timer on Compare match (CTC) mode
-	TIMSK1 |= (1<<OCIE1A); //Timer/Counter1 Interrupt Mask Register = Bit 1 – OCIE1A: Timer/Counter1, Output Compare A Match Interrupt Enable
-	OCR1A = (F_CPU/(freqHz * 2 * 256) -1); //Output Compare Registers = F_CPU/frequency*2*N-1 //see 18.7.2 Clear Timer on Compare Match (CTC) Mode in datasheet
+	LED_PB0_OFF;
 }
+
+
+//void PWM_Initialise(uint16_t freqHzCompA, uint16_t freqHzCompB)
+//{
+	//TCCR1B |= (1<<CS10) | (1<<WGM12); // Timer/Counter Control Register B = No prescaler) |  Clear Timer on Compare match (CTC) mode
+	//TIMSK1 |= (1<<OCIE1A) | (1<<OCIE1B); //Timer/Counter1 Interrupt Mask Register = Bit 1 – OCIE1A: Timer/Counter1, Output Compare A Match Interrupt Enable | Bit 2 – OCIE1B: Timer/Counter1, Output Compare B Match Interrupt Enable
+	//
+	////Max pulse length = 65536 (16 bit register size) / 8 Mhz (max clock speed) = 0.008192s (no prescaler). 
+	////Min Frequency = 122Hz (with OCR1A/OCR1B set to 0xFFFF)
+	////Max frequency = 8Mhz (with OCR1A/OCR1B set to 0x0001) 
+	//OCR1A = (F_CPU/freqHzCompA) -1; //Output Compare Registers = F_CPU/frequency*N-1 //see 18.7.2 Clear Timer on Compare Match (CTC) Mode in datasheet
+	//OCR1B = (F_CPU/freqHzCompB) -1; //Output Compare Registers = F_CPU/frequency*N-1 //see 18.7.2 Clear Timer on Compare Match (CTC) Mode in datasheet
+//}
+
+//dutyCyclePercentage between 0 and 100
+//BUG for 0 and >=100%
+void PWM_Initialise(uint8_t dutyCyclePercentage)
+{
+	TCCR1B |= (1<<CS10) | (1<<WGM12); // Timer/Counter Control Register B = No prescaler) |  Clear Timer on Compare match (CTC) mode
+	TIMSK1 |= (1<<OCIE1A) | (1<<OCIE1B); //Timer/Counter1 Interrupt Mask Register = Bit 1 – OCIE1A: Timer/Counter1, Output Compare A Match Interrupt Enable | Bit 2 – OCIE1B: Timer/Counter1, Output Compare B Match Interrupt Enable
+	
+	uint16_t comp = 800; //10Khz  (F_CPU/freqHzCompA) 
+	uint16_t compA = comp;
+	uint16_t compB =  (dutyCyclePercentage/100.0) * comp;
+	
+	//these don't quite work
+	if (dutyCyclePercentage == 0)
+	{
+		compA=comp;
+		compB=0;
+	}
+	//these don't quite work
+	if (dutyCyclePercentage >=100)
+	{
+		compA=comp;
+		compB=comp;
+	}
+	
+	//Max pulse length = 65536 (16 bit register size) / 8 Mhz (max clock speed) = 0.008192s (no prescaler).
+	//Min Frequency = 122Hz (with OCR1A/OCR1B set to 0xFFFF)
+	//Max frequency = 8Mhz (with OCR1A/OCR1B set to 0x0001)
+	//OCR1A= 1 = 8Mhz
+	//OCR1A= 2 = 4Mhz
+	//OCR1A= 100 = 800Khz, //OCR1A= 400 = 200Khz,//OCR1A= 800 = 100Khz
+	
+	//The TOP value is the value stored in the OCR1A Register. Reaching it will cause the counter to reset
+	OCR1A = compA; //ON: Output Compare Registers = F_CPU/frequency*N-1 //see 18.7.2 Clear Timer on Compare Match (CTC) Mode in datasheet
+	OCR1B = compB; //OFF: Output Compare Registers = F_CPU/frequency*N-1 //see 18.7.2 Clear Timer on Compare Match (CTC) Mode in datasheet
+	
+	//Larger OCR1B is compared to OCR1A, the larger the duty cycle. The Off frequency is reducing as OCR1B increases.
+}
+
 
 int main(void)
 {
@@ -43,7 +94,19 @@ int main(void)
 	DDRB |= (1<<DDB0); // Data direction register - Set PB0 high for output
 	DDRB &= ~(1<<DDB7); //Data direction register - Set PB7 low for input
 	
-	Set_Timer1_Interrupt_Frequency(1);
+	
+	//PWM_Initialise(12); //12% Duty Cycle
+	//PWM_Initialise(6); //6% Duty Cycle
+	//PWM_Initialise(1); //1% Duty Cycle
+	//PWM_Initialise(25); //25% Duty Cycle
+	//PWM_Initialise(50); //50% Duty Cycle
+	//PWM_Initialise(75); //75% Duty Cycle
+	
+	//PWM_Initialise(100); //100% Duty Cycle //these don't quite work
+	//PWM_Initialise(0); //0% Duty Cycle  //these don't quite work
+	
+	PWM_Initialise(75); 
+	
 
 	sei(); //Enable interrupts by setting the global interrupt mask
 	
---
+++ Getting Started with AVR/AVR_USART with Interrupts/AVR_USART/main.c
@@ -0,0 +1,189 @@
+
+
+#define FOSC 8000000 // Clock Speed
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+/* USART Buffer Defines */
+#define USART_RX_BUFFER_SIZE 128     /* 2,4,8,16,32,64,128 or 256 bytes */
+#define USART_TX_BUFFER_SIZE 128     /* 2,4,8,16,32,64,128 or 256 bytes */
+#define USART_RX_BUFFER_MASK (USART_RX_BUFFER_SIZE - 1)
+#define USART_TX_BUFFER_MASK (USART_TX_BUFFER_SIZE - 1)
+
+#if (USART_RX_BUFFER_SIZE & USART_RX_BUFFER_MASK)
+#error RX buffer size is not a power of 2
+#endif
+#if (USART_TX_BUFFER_SIZE & USART_TX_BUFFER_MASK)
+#error TX buffer size is not a power of 2
+#endif
+
+/* Static Variables */
+static unsigned char USART_RxBuf[USART_RX_BUFFER_SIZE];
+static volatile unsigned char USART_RxHead;
+static volatile unsigned char USART_RxTail;
+static unsigned char USART_TxBuf[USART_TX_BUFFER_SIZE];
+static volatile unsigned char USART_TxHead;
+static volatile unsigned char USART_TxTail;
+
+/* Prototypes */
+void USART_Init(unsigned int ubrr_val);
+unsigned char USART_Receive(void);
+void USART_Transmit(unsigned char data);
+
+/* Initialize USART */
+void USART_Init(unsigned int ubrr_val)
+{
+	unsigned char x;
+
+	/* Set the baud rate */
+	UBRR0H = (unsigned char) (ubrr_val>>8);
+	UBRR0L = (unsigned char) ubrr_val;
+	
+	/* Enable USART receiver and transmitter */
+	UCSR0B = ((1 << RXCIE0) | (1 << RXEN0) | (1 << TXEN0));
+
+	///* Set frame format: 8 data, 1 stop bit */
+	UCSR0C = (1<<UCSZ01) | (1<<UCSZ00) ;
+	
+	/* Flush receive buffer */
+	x = 0;
+
+	USART_RxTail = x;
+	USART_RxHead = x;
+	USART_TxTail = x;
+	USART_TxHead = x;
+}
+
+int main(void)
+{
+	
+	USART_Init(MYUBRR); //Calculate baud rate
+	sei();
+
+	for( ; ; ) {
+		/* Echo the received character */
+		USART_Transmit(USART_Receive());
+	}
+	
+	return 0;
+}
+
+
+
+ISR(USART_RX_vect)
+{
+	unsigned char data;
+	unsigned char tmphead;
+
+	/* Read the received data */
+	data = UDR0;
+	/* Calculate buffer index */
+	tmphead = (USART_RxHead + 1) & USART_RX_BUFFER_MASK;
+	/* Store new index */
+	USART_RxHead = tmphead;
+
+	if (tmphead == USART_RxTail)
+	{
+		/* ERROR! Receive buffer overflow */
+	}
+	/* Store received data in buffer */
+	USART_RxBuf[tmphead] = data;
+}
+
+
+
+ISR(USART_UDRE_vect)
+{
+	unsigned char tmptail;
+
+	/* Check if all data is transmitted */
+	if (USART_TxHead != USART_TxTail)
+	{
+		/* Calculate buffer index */
+		tmptail = (USART_TxTail + 1) & USART_TX_BUFFER_MASK;
+		/* Store new index */
+		USART_TxTail = tmptail;
+		/* Start transmission */
+		UDR0 = USART_TxBuf[tmptail];
+	}
+	else
+	{
+		/* Disable UDRE interrupt */
+		UCSR0B &= ~(1<<UDRIE0);
+	}
+}
+unsigned char USART_Receive(void)
+{
+	unsigned char tmptail;
+	
+	/* Wait for incoming data */
+	while (USART_RxHead == USART_RxTail);
+	/* Calculate buffer index */
+	tmptail = (USART_RxTail + 1) & USART_RX_BUFFER_MASK;
+	/* Store new index */
+	USART_RxTail = tmptail;
+	/* Return data */
+	return USART_RxBuf[tmptail];
+}
+
+void USART_Transmit(unsigned char data)
+{
+	unsigned char tmphead;
+	
+	/* Calculate buffer index */
+	tmphead = (USART_TxHead + 1) & USART_TX_BUFFER_MASK;
+	/* Wait for free space in buffer */
+	while (tmphead == USART_TxTail);
+	/* Store data in buffer */
+	USART_TxBuf[tmphead] = data;
+	/* Store new index */
+	USART_TxHead = tmphead;
+	/* Enable UDRE interrupt */
+	UCSR0B |= (1<<UDRIE0);
+}
+
+
+
+//void USART_Init( unsigned int ubrr)
+//{
+///*Set baud rate */
+//UBRR0H = (unsigned char)(ubrr>>8);
+//UBRR0L = (unsigned char)ubrr;
+//
+////Enable receiver and transmitter
+//UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+//
+///* Set frame format: 8data, 1 stop bit */
+//UCSR0C = (3<<UCSZ00);
+//}
+//
+//unsigned char USART_Receive( void )
+//{
+///* Wait for data to be received */
+//while ( !(UCSR0A & (1<<RXC0)) );
+///* Get and return received data from buffer */
+//return UDR0;
+//}
+//
+//void USART_Transmit( unsigned char data )
+//{
+///* Wait for empty transmit buffer */
+//while ( !( UCSR0A & (1<<UDRE0)) );
+///* Put data into buffer, sends the data */
+//UDR0 = data;
+//}
+//
+//int main(void)
+//{
+//USART_Init(MYUBRR);
+//
+///* Replace with your application code */
+//while (1)
+//{
+//USART_Transmit(USART_Receive());
+//}
+//}
+
