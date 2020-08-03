a8a90442b7b13ca40ca234c1ae703253b4c628cb
John Scully <j0hns@users.noreply.github.com>
2020-08-03 17:41:33 +0100

    Simple echo whatever was sent

--- Getting Started with AVR/Useful Serial/AVR_USART/main.c
+++ Getting Started with AVR/Useful Serial/AVR_USART/main.c
@@ -1,7 +1,7 @@
 
 #define F_CPU 8000000 // Clock Speed
 #define BAUD 9600
-#define MYUBRR F_CPU/16/BAUD-1
+#define UBRR_VALUE F_CPU/16/BAUD-1
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -9,31 +9,42 @@
 #include <util/delay.h>
 #include <stdio.h>
 
-char helloWorldString[]="Hello World!!";
 
 
 static FILE this_stdout = FDEV_SETUP_STREAM(USART_Transmit_IO, USART_Receive_IO,  _FDEV_SETUP_WRITE);
 
+
+
 int main(void)
 {
 	
-	USART_Init(MYUBRR); //Calculate baud rate
+	USART_Init(UBRR_VALUE); //Calculate baud rate
 	sei();
 	
 	stdin=stdout=&this_stdout;
 	
-	uint16_t u16Data = 10;
-
-	for( ; ; ) {
-		/* Echo the received character */
-		//USART_Transmit(USART_Receive());
-		USART_TransmitString(helloWorldString);
-		printf("\nunsigned int = %u",u16Data);
-		_delay_ms(1000);
+	//char receivedString[USART_RX_BUFFER_SIZE];
+	uint8_t u8Data;
+	while(1)
+	{
+		USART_Transmit(USART_Receive());
+		/*
+		printf("\nPress any key:");
+		//scan standard stream (USART)
+		scanf("%c",&u8Data);
+		printf("\nYou pressed: ");
+		//print scanned character and its code
+		printf("%c; Key code: %u",u8Data, u8Data);
+		//printf("\nEnter a string and press Return:");
+		//gets(&receivedString);
+		//printf("\nYou entered:'%s'",receivedString);
+		*/
 	}
 	
 	return 0;
 }
+
+
 
 
 
